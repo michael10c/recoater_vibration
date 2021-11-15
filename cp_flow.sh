994c9d3932fea0701ecb49c6639fc6b5fb9a5f86
Michael Carrillo <michaelfcarrillo@gmail.com>
2021-11-15 23:47:12 +0000

    added node_red flows and the bash script to cp them

---
+++ cp_flow.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cp /var/lib/node-red/.node-red/flows_beaglebone.json ~/recoater_vibration/node_red/flows_beaglebone.json
+cp /var/lib/node-red/.node-red/settings.js ~/recoater_vibration/node_red/settings.js
+cp /var/lib/node-red/.node-red/package.json ~/recoater_vibration/node_red/package.json
