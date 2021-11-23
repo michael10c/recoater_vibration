e6b52007a7f3f76b2ce0ced4427e0221536e8bd7
Michael Carrillo <michaelfcarrillo@gmail.com>
2021-11-23 22:07:00 +0000

    git changes: moved directory for nodered ADC to correct git directory made folders for PRU code made user inputs for freq and # of samples added new group for configuration labeled buttons and chart titles
    what needs to happen:
    add matplotlib or fix the error by taking it out

--- cp_flow.sh
+++ cp_flow.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cp /var/lib/node-red/.node-red/flows_beaglebone.json ~/recoater_vibration/node_red/flows_beaglebone.json
-cp /var/lib/node-red/.node-red/settings.js ~/recoater_vibration/node_red/settings.js
-cp /var/lib/node-red/.node-red/package.json ~/recoater_vibration/node_red/package.json
+cp /var/lib/node-red/.node-red/flows_beaglebone.json /home/debian/recoater_vibration/node_red/flows_beaglebone.json
+cp /var/lib/node-red/.node-red/settings.js /home/debian/recoater_vibration/node_red/settings.js
+cp /var/lib/node-red/.node-red/package.json /home/debian/recoater_vibration/node_red/package.json
