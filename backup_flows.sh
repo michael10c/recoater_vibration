dbf7e72196e82d9791e34902801c5d42413aee31
Michael Carrillo <michaelfcarrillo@gmail.com>
2022-01-27 03:22:13 +0000

    idk

---
+++ backup_flows.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp /home/debian/recoater_vibration/node_red/flows_beaglebone.json /var/lib/node-red/.node-red/flows_beaglebone.json
+cp /home/debian/recoater_vibration/node_red/settings.js /var/lib/node-red/.node-red/settings.js
+cp /home/debian/recoater_vibration/node_red/package.json /var/lib/node-red/.node-red/package.json
+
