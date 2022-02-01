abe16c2168ca9ba791fdff5924491a4338ca121c
Michael Carrillo <michaelfcarrillo@gmail.com>
2022-02-01 11:22:37 -0500

    aditing python code to display critical info after each recoat. disabled plots to speed up code

--- signal_analysis.py
+++ signal_analysis.py
@@ -1,15 +1,12 @@
 import numpy as np
 import json
 from matplotlib import pyplot as plt
-# rfft: only real side
-#from scipy.fft import rfft, rfftfreq
 from scipy.fftpack import rfft, rfftfreq
 import datetime
 
 
 def read_values():
-    #  read_values() returns data from json file
-
+    # read_values() returns data from json file
     # grab number of samples (sample size) from data
     sampleSize = data['sampleSize']
     # grab period ( sampling interval) from data
@@ -20,23 +17,22 @@ def read_values():
     return sampleSize, samplingInterval, values
 
 
-# cannot REUSE NAME!!!!!
+def read_file():
+    # reads json file
+    with open(accelFilePath) as f:
+        data_temp = json.load(f)
+
+    return data_temp
+
 
 # Reading data, assign path to variable
+# X axis ############
 # On BBB
 accelFilePath = '/home/debian/recoater_vibration/data/testing_code/x_axis.txt'
-# accelFilePath = '/var/lib/node-red/frfRawData/force.txt'
-
-## X axis
 # on PC
 # accelFilePath = "C:\\Users\\Michael\\Documents\\master\\data\\testing_code\\x_axis.txt"
 
-# start list, do I need this?
-# accelSignal = []
-
-# open file above and load the json object into data variable
-with open(accelFilePath) as f:
-    data = json.load(f)
+data = read_file()
 
 # obtain values from file above and save into variables
 number_of_samples, period, accelSignal = read_values()
@@ -52,16 +48,14 @@ accelSignal = [float(i) for i in accelSignal]
 # convert list to array
 array_accel_x = np.array(accelSignal)
 
-## Y axis
+# Y axis##########################
 # on PC
 # accelFilePath = "C:\\Users\\Michael\\Documents\\master\\data\\testing_code\\y_axis.txt"
-
 # on BBB
 accelFilePath = '/home/debian/recoater_vibration/data/testing_code/y_axis.txt'
 
 # open file above and load the json object into data variable
-with open(accelFilePath) as f:
-    data = json.load(f)
+data = read_file()
 
 # obtain values from file above and save into variables
 number_of_samples, period, accelSignal = read_values()
@@ -108,99 +102,82 @@ for i in range(len(array_accel)):
     norm_center[i] = (accel_centered[i] / max_accel_centered[i])
 
 # RMS of signal
-rms = np.sqrt(np.mean(norm_center ** 2))
-# print(normalized_accel)
-# print(accelSignal_center)
+rms = np.sqrt(np.mean((norm_center ** 2), axis=1))
 print("RMS = ", rms)
 
-# time = []
-# for i in range(0, len(accelSignal), 1):
-#    time.append(i * period)
-
-# my_fig = plt.figure(figsize=(10.0, 4.0))
-# time = my_fig.add_subplot(2, 2, 1)
-# time_c = my_fig.add_subplot(2, 2, 2)
-# time_cn = my_fig.add_subplot(2, 2, 3)
-# freq = my_fig.add_subplot(2, 2, 4)
-
-
-
-# time domain
-plot1 = plt.figure("Time Domain X Axis")
-plt.title('Time vs Amplitude')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
-plt.plot(array_accel[0])
-plot2 = plt.figure("Time Domain Y Axis")
-plt.title('Time vs Amplitude')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
-plt.plot(array_accel[1])
-
-plot3 = plt.figure("Time Domain 3")
-plt.title('Time vs Amplitude, centered  X Axis')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
-plt.plot(accel_centered[0])
-plot4 = plt.figure("Time Domain 4")
-plt.title('Time vs Amplitude, centered  Y Axis')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
-plt.plot(accel_centered[1])
-
-plot5 = plt.figure("Time Domain 5")
-plt.title('Time vs Amplitude, Normalized and Centered  X Axis')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
-plt.plot(norm_center[0])
-plot6 = plt.figure("Time Domain 6")
-plt.title('Time vs Amplitude, Normalized and Centered  Y Axis')
-plt.xlabel('Time (ms)')
-plt.ylabel('Amplitude')
-plt.plot(norm_center[1])
+# # time domain
+# plot1 = plt.figure("Time Domain X Axis")
+# plt.title('Time vs Amplitude')
+# plt.xlabel('Time (ms)')
+# plt.ylabel('Amplitude')
+# plt.plot(array_accel[0])
+# plot2 = plt.figure("Time Domain Y Axis")
+# plt.title('Time vs Amplitude')
+# plt.xlabel('Time (ms)')
+# plt.ylabel('Amplitude')
+# plt.plot(array_accel[1])
+#
+# plot3 = plt.figure("Time Domain 3")
+# plt.title('Time vs Amplitude, centered  X Axis')
+# plt.xlabel('Time (ms)')
+# plt.ylabel('Amplitude')
+# plt.plot(accel_centered[0])
+# plot4 = plt.figure("Time Domain 4")
+# plt.title('Time vs Amplitude, centered  Y Axis')
+# plt.xlabel('Time (ms)')
+# plt.ylabel('Amplitude')
+# plt.plot(accel_centered[1])
+#
+# plot5 = plt.figure("Time Domain 5")
+# plt.title('Time vs Amplitude, Normalized and Centered  X Axis')
+# plt.xlabel('Time (ms)')
+# plt.ylabel('Amplitude')
+# plt.plot(norm_center[0])
+# plot6 = plt.figure("Time Domain 6")
+# plt.title('Time vs Amplitude, Normalized and Centered  Y Axis')
+# plt.xlabel('Time (ms)')
+# plt.ylabel('Amplitude')
+# plt.plot(norm_center[1])
 
 # calc FFT
 # normalize FFT and * 2 to get same magnitude as time domain
-y_mag = (rfft(norm_center) / number_of_samples) * 2
-x_freq = rfftfreq(number_of_samples, period)
+magnitude_FFT = (rfft(norm_center) / number_of_samples) * 2
+frequency_FFT = rfftfreq(number_of_samples, period)
+
+print("Maximum acceleration: ", max_accel, "m/s^2")
+print("Minimum acceleration: ", min_accel, "m/s^2")
+print("Frequency of maximum magnitude: ", frequency_FFT[np.argmax(magnitude_FFT, axis=1)], "Hz")
 
 # freq domains
 plot7 = plt.figure("Frequency Domain 7")
 plt.title('Frequency vs Magnitude, Normalized')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude')
-plt.plot(x_freq, np.abs(y_mag[0]))
+plt.plot(frequency_FFT, np.abs(magnitude_FFT[0]))
 plot8 = plt.figure("Frequency Domain 8")
 plt.title('Frequency vs Magnitude, Normalized')
 plt.xlabel('Frequency (Hz)')
 plt.ylabel('Magnitude')
-plt.plot(x_freq, np.abs(y_mag[1]))
+plt.plot(frequency_FFT, np.abs(magnitude_FFT[1]))
 plt.show()
 
-
 #  Convert output to list for creating json file
-x_freq = list(x_freq[])
-
-x_axis_magnitude = y_mag.tolist()
-y_mag = list(y_mag[])
-
+frequency_FFT = frequency_FFT.tolist()
+x_axis_magnitude = np.abs(magnitude_FFT.tolist()[0])
+y_axis_magnitude = np.abs(magnitude_FFT.tolist()[1])
 
 # Define output file
 jsonFile = {
-    "frequency": x_freq,
-    "magnitude": x_axis_magnitude,
+    "frequency": frequency_FFT,
+    "x_axis_magnitude": x_axis_magnitude,
+    "y_axis_magnitude": y_axis_magnitude,
     "standardDeviation": std_dev
 }
 
+# On BBB
+saveDataFilepath = '/home/debian/recoater_vibration/data/testing_code/processed_accel.txt'
 # On laptop
 # saveDataFilepath = "C:\\Users\\Michael\\Documents\\master\\data\\testing_code\\x_axis_fft.txt"
-
-# push
-
-# On BBB
-# saveDataFilepath = '/var/lib/node-red/frfRawData/frf.txt'
-saveDataFilepath = '/home/debian/recoater_vibration/data/testing_code/processed_accel.txt'
-
 
 f = open(saveDataFilepath, "w")
 f.write(str(jsonFile))
@@ -208,23 +185,6 @@ f.close()
 
 print("Hello from BBB")
 print("Timestamp: ", datetime.datetime.now())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
